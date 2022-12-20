fc0fe5f6f6cb3f845128e802faf39722ab692774
FillipeW <welausen@gmail.com>
2022-12-20 15:23:23 -0300

    melhorias no layout

--- PI/BeatLab/beatLab2/public/javascripts/signup.js
+++ PI/BeatLab/beatLab2/public/javascripts/signup.js
@@ -1,63 +1,119 @@
 const infCadastro = document.querySelector('#formUp');
 
 infCadastro.onsubmit = evento => {
+    var campoNome = false
+    var campoLastName = false
+    var campoEmail = false
+    var campoSenha = false 
+
     //receber o valor do campo 
     var nome = document.querySelector('#name');
-    var email = document.querySelector('#email').value;
-    var password = document.querySelector('#password').value;
-    var repetPassword = document.querySelector('#repetPassword').value;
-    //verificar se o campo esta em branco 
-   if(!nome.value) {
-   evento.preventDefault();
-    document.getElementById('msgAlerta').innerHTML = '<p> Hum...Algo deu errado.</p><p> Campo Nome é obrigatorio!</p>'
-   
-    return;
-}
-    //receber o valor do campo 
-    
-    //verificar se o campo esta em branco 
-    if(email === "") {
-    evento.preventDefault();
-    document.getElementById('msgAlerta').innerHTML = '<p> Hum...Algo deu errado.</p><p>Campo Email é obrigatorio!</p>'
-    return;
-}
-    //receber o valor do campo 
-    
-    if(repetEmail === '') {
-    evento.preventDefault();
-    document.getElementById('msgAlerta').innerHTML = '<p> Hum...Algo deu errado.</p><p>Você precisa confirmar seu email!</p>'
-    return;
-}
-    //receber o valor do campo 
-    
-    //verificar se o campo esta em branco 
-    if(password === '') {
-    evento.preventDefault();
-    document.getElementById('msgAlerta').innerHTML = '<p> Hum...Algo deu errado.</p><p>Você Não pode esquecer da sua senha!</p>'
-    return;
-}
- //receber o valor do campo 
- 
- //verificar se o campo esta em branco 
- if(repetPassword === '') {
- evento.preventDefault();
- document.getElementById('msgAlerta').innerHTML = '<p> Hum...Algo deu errado?</p><p>Você precisa repetir sua senha!</p>'
- return;
+    var lastName = document.querySelector('#lastName');
+    var email = document.querySelector('#email');
+    var repetEmail = document.querySelector('#repetEmail');
+    var password = document.querySelector('#password');
+    var repetPassword = document.querySelector('#repetPassword');
 
-} 
+ // Valida os campos de senha!
+ if(password.value !== repetPassword.value || !password.value || !repetPassword || password.value.length < 6 || password.value.length > 10) {
+   
+   if( password.value.length < 6 || password.value.length > 10){
+    document.getElementById('msgAlerta').innerHTML = '<p>Sua senha precisa ter de 6 a 10 caracteres!</p>'
+   }else{
+    document.getElementById('msgAlerta').innerHTML = '<p>Você precisa repetir sua senha!</p>'
+   } 
+   
+    campoSenha = false   
+} else{
+    campoSenha = true
+}
+
+
+
+// Valida os campos de emails
+    if(!email.value) {
+        document.getElementById('msgAlerta').innerHTML = '<p>Campo Email é obrigatorio!</p>'
+        campoEmail = false
+    }
+        
+
+    if(email.value !== repetEmail.value || !email.value || !repetEmail.value){
+        document.getElementById('msgAlerta').innerHTML = '<p>Os campos de email percisam ser iguais!</p>'
+        campoEmail = false
+    }else{
+        campoEmail = true
+    }
+
+
+
+// Valida os campos de Nome e Sobrenome
+  if(!lastName.value) {
+    document.getElementById('msgAlerta').innerHTML = '<p> Campo Sobrenome é obrigatório!</p>'
+    campoLastName = false
+}else{
+   campoLastName = true
+}
+
+    if(!nome.value) {
+        document.getElementById('msgAlerta').innerHTML = '<p> Campo Nome é obrigatório!</p>'
+        campoNome = false
+    }else{
+        campoNome = true
+    }
+
+
+// verifica se todos os campos estão devidamente preenchidos  
+
+if(campoNome == true && campoLastName == true && campoEmail == true && campoSenha == true ){
+     console.log('enviado**')
+     windown.alert('enviado**')
+     evento.preventDefault();
+//     window.location.href = "/home";
+
+  
+}else{
+    evento.preventDefault();
+}
+
 };
 
 
 //JANELA MODAL PARA VOLTAR AO HOME APOS FINALIZAR O CADASTRO
-// function finalizar(){
+function finalizar(cadastroOk){
     
-//     const modal = document.getElementById('janela-modal')
-       
-//     modal.classList.add('abrir') 
+    if(cadastroOk == true){
+      const modal = document.getElementById('janela-modal')     
+      modal.classList.add('abrir') 
+      modal.addEventListener('click', (e) => {
+        if(e.target.id == 'home-bt' ){
+         modal.classList.remove('abrir')     
+     }          
+  }) 
+ 
+};
 
-//     modal.addEventListener('click', (e) => {
-//         if(e.target.id == 'home-bt' ){
-//             modal.classList.remove('abrir')
-//         }         
-//     }) 
-//};
+}
+
+
+
+
+// function enviar(){
+//     return envia=true
+// }
+
+
+// setInterval(function(){
+// if(contador > 0){
+//     decrementaContador()
+// }
+// },
+// 1000);
+
+
+// function decrementaContador(){
+//     var contadorLabel = document.getElementById('contador')
+//     contador --
+//     contadorLabel.innerHTML = 'Você será redirecionado em ' + contador
+//     console.log(contador)
+
+// }
--- PI/BeatLab/beatLab2/src/routes/listaDeProdutos.js
+++ PI/BeatLab/beatLab2/src/routes/listaDeProdutos.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var coletanea = require('../database/repertorio.json')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('listaDeProdutos', { title: 'Express' });
+  res.render('listaDeProdutos', { title: 'Express', coletanea });
 });
 
 module.exports = router;
